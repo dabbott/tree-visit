cb542fe540c1ed6c702eb0664243f5301f56549e
Devin Abbott <devinabbott@gmail.com>
2023-10-27 14:46:44 -0700

    All functions defined

--- src/defineTree.ts
+++ src/defineTree.ts
@@ -1,17 +1,28 @@
 import { access, accessPath } from './access'
 import { DiagramOptions, diagram } from './diagram'
+import {
+  FindOptions,
+  find,
+  findAll,
+  findAllIndexPaths,
+  findIndexPath,
+} from './find'
+import { flat } from './flat'
+import { FlatMapOptions, flatMap } from './flatMap'
 import { IndexPath } from './indexPath'
 import { InsertOptions, insert } from './insert'
+import { MapOptions, map } from './map'
+import { MoveOptions, move } from './move'
 import { BaseOptions, MutationBaseOptions } from './options'
+import { ReduceOptions, reduce } from './reduce'
 import { RemoveOptions, remove } from './remove'
+import { ReplaceOptions, replace } from './replace'
 import { ExtractRequiredKeys, OptionCheck } from './types'
+import { VisitOptions, visit } from './visit'
 
 type WithoutBase<T> = Omit<T, keyof BaseOptions<T>>
 
 type MutationOptions<T> = WithoutBase<MutationBaseOptions<T>>
-
-type InsertOptionsWB<T> = WithoutBase<InsertOptions<T>>
-type RemoveOptionsWB<T> = WithoutBase<RemoveOptions<T>>
 
 type DiagramOptionsWB<T> = WithoutBase<DiagramOptions<T>>
 type DiagramRequiredOptions<T> = Pick<
@@ -22,6 +33,12 @@ type DiagramOptionalOptions<T> = Omit<
   DiagramOptionsWB<T>,
   ExtractRequiredKeys<DiagramOptionsWB<T>>
 >
+type FindOptionsWB<T> = WithoutBase<FindOptions<T>>
+type VisitOptionsWB<T> = WithoutBase<VisitOptions<T>>
+type InsertOptionsWB<T> = WithoutBase<InsertOptions<T>>
+type RemoveOptionsWB<T> = WithoutBase<RemoveOptions<T>>
+type MoveOptionsWB<T> = WithoutBase<MoveOptions<T>>
+type ReplaceOptionsWB<T> = WithoutBase<ReplaceOptions<T>>
 
 type ApplyableOptions<T> = DiagramRequiredOptions<T> & MutationOptions<T>
 
@@ -38,42 +55,113 @@ class Tree<T, AppliedOptions extends Partial<ApplyableOptions<T>>> {
 
   public baseOptions: BaseOptions<T>
 
+  private mergeOptions = <T extends Record<string, any>>(options: T) => ({
+    ...this.baseOptions,
+    ...this.appliedOptions,
+    ...options,
+  })
+
+  withOptions = <NewOptions extends Partial<ApplyableOptions<T>>>(
+    newOptions: NewOptions
+  ) => new Tree(this.baseOptions, { ...this.appliedOptions, ...newOptions })
+
   get getChildren() {
     return this.baseOptions.getChildren
   }
 
   access = (node: T, indexPath: IndexPath) =>
-    access(node, indexPath, this.baseOptions)
+    access(node, indexPath, this.mergeOptions({}))
 
   accessPath = (node: T, indexPath: IndexPath) =>
-    accessPath(node, indexPath, this.baseOptions)
+    accessPath(node, indexPath, this.mergeOptions({}))
 
   diagram = (
     node: T,
-    options: AppliedOptions extends DiagramRequiredOptions<T>
-      ? DiagramOptionalOptions<T> | void
-      : OptionCheck<AppliedOptions, 'getLabel', DiagramRequiredOptions<T>> &
-          DiagramOptionalOptions<T>
+    options:
+      | DiagramRequiredOptions<T>['getLabel']
+      | (AppliedOptions extends DiagramRequiredOptions<T>
+          ? DiagramOptionalOptions<T> | void
+          : OptionCheck<AppliedOptions, 'getLabel', DiagramRequiredOptions<T>> &
+              DiagramOptionalOptions<T>)
   ) =>
-    diagram(node, { ...this.baseOptions, ...this.appliedOptions, ...options })
+    typeof options === 'function'
+      ? diagram(node, { ...this.baseOptions, getLabel: options })
+      : diagram(node, this.mergeOptions(options))
+
+  find = (node: T, options: FindOptionsWB<T>) =>
+    find(node, this.mergeOptions(options))
+
+  findAll = (node: T, options: FindOptionsWB<T>) =>
+    findAll(node, this.mergeOptions(options))
+
+  findIndexPath = (node: T, options: FindOptionsWB<T>) =>
+    findIndexPath(node, this.mergeOptions(options))
+
+  findAllIndexPaths = (node: T, options: FindOptionsWB<T>) =>
+    findAllIndexPaths(node, this.mergeOptions(options))
+
+  flat = (node: T) => flat(node, this.mergeOptions({}))
+
+  flatMap = <R>(node: T, transform: FlatMapOptions<T, R>['transform']) =>
+    flatMap(node, this.mergeOptions({ transform }))
+
+  reduce = <R>(
+    node: T,
+    nextResult: ReduceOptions<T, R>['nextResult'],
+    initialResult: R
+  ): R => reduce(node, this.mergeOptions({ nextResult, initialResult }))
+
+  map = <R>(node: T, transform: MapOptions<T, R>['transform']): R =>
+    map(node, this.mergeOptions({ transform }))
+
+  // --- Mutations ---
+
+  /**
+   * Visit each node using preorder DFS traversal.
+   */
+  visit(node: T, onEnter: NonNullable<VisitOptions<T>>['onEnter']): void
+
+  /**
+   * Visit each node using DFS traversal.
+   */
+  visit(node: T, options: VisitOptionsWB<T>): void
+
+  visit(
+    node: T,
+    onEnterOrOptions:
+      | NonNullable<VisitOptions<T>>['onEnter']
+      | VisitOptionsWB<T>
+  ) {
+    if (typeof onEnterOrOptions === 'function') {
+      visit(node, this.mergeOptions({ onEnter: onEnterOrOptions }))
+    } else {
+      visit(node, this.mergeOptions({ ...onEnterOrOptions }))
+    }
+  }
 
   insert = (
     node: T,
     options: OptionCheck<AppliedOptions, 'create', InsertOptionsWB<T>> &
       Omit<InsertOptionsWB<T>, 'create'>
-  ) => insert(node, { ...this.baseOptions, ...this.appliedOptions, ...options })
+  ) => insert(node, this.mergeOptions(options))
 
   remove = (
     node: T,
     options: OptionCheck<AppliedOptions, 'create', RemoveOptionsWB<T>> &
       Omit<RemoveOptionsWB<T>, 'create'>
-  ) => remove(node, { ...this.baseOptions, ...this.appliedOptions, ...options })
+  ) => remove(node, this.mergeOptions(options))
 
-  // ---
+  move = (
+    node: T,
+    options: OptionCheck<AppliedOptions, 'create', MoveOptionsWB<T>> &
+      Omit<MoveOptionsWB<T>, 'create'>
+  ) => move(node, this.mergeOptions(options))
 
-  withOptions = <NewOptions extends Partial<ApplyableOptions<T>>>(
-    newOptions: NewOptions
-  ) => new Tree(this.baseOptions, { ...this.appliedOptions, ...newOptions })
+  replace = (
+    node: T,
+    options: OptionCheck<AppliedOptions, 'create', ReplaceOptionsWB<T>> &
+      Omit<ReplaceOptionsWB<T>, 'create'>
+  ) => replace(node, this.mergeOptions(options))
 }
 
 export function defineTree<T>(
@@ -91,6 +179,7 @@ const a = { label: 'a', children: [] }
 const TestTree1 = defineTree((node: TestNode) => node.children)
 TestTree1.diagram(a, { getLabel: (node) => node.label })
 // TestTree1.diagram(a, {}) // Fails
+TestTree1.diagram(a, (node) => node.label)
 
 const TestTree2 = TestTree1.withOptions({ getLabel: (node) => node.label })
 // TestTree2.diagram(a)
